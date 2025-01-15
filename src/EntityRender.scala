91dc9000dd1c05805e5787b911ee12a61c5f5e0a
louis272 <louis.schanen@gmail.com>
2025-01-15 11:00:35 +0100

    Comment EntityRender

--- src/EntityRender.scala
+++ src/EntityRender.scala
@@ -1,11 +1,14 @@
 /**
  * EntityRender class
+ * This class generalizes the rendering of entities.
+ *
  * @param frames List of frames
  * @param scale Scale of the entity
  */
 class EntityRender(var frames: List[String], var scale : Double) {
   /**
    * Constructor for EntityRender
+   *
    * @param scale Scale of the entity
    */
   def this(scale: Double) = this(List(), scale)
